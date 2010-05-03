71417f44f0709b48b9bac92647a44902ef8c5643
SND\aliaksandr_pedzko_cp <SND\aliaksandr_pedzko_cp@a83551a4-30f6-4d81-a974-c6ced450ddbf>
2010-05-03 08:59:10 +0000

    Small example of API usage was added.
    git-svn-id: https://kpnet.svn.codeplex.com/svn/trunk/KpNet@43181 a83551a4-30f6-4d81-a974-c6ced450ddbf

---
+++ Example/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+using System;
+using System.Data;
+using KpNet.KdbPlusClient;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main()
+        {
+            //1. download trial kdb+ here http://kx.com/Developers/software.php
+            //2. start q process on 1001 port by running  'q -p 1001'
+                      
+            try
+            {
+                using (IDatabaseConnection connection = new KdbDatabaseConnection("localhost", 1001))
+                {
+                    // get current time
+                    TimeSpan time = connection.ExecuteScalar<TimeSpan>(".z.T");
+                    Console.WriteLine("Current time {0}",time);
+
+                    // create trade table
+                    connection.ExecuteNonQuery("trade:([]time:();sym:();price:();size:())");
+
+                    // get list of tables in db
+                    // should output 'trade'
+                    IDataReader reader = connection.ExecuteQuery("\\a");
+                    Console.WriteLine("Tables in database:");
+
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader.GetString(0));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
