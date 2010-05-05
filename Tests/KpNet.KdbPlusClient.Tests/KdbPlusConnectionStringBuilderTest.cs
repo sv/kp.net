ee713590de1248d193e510f1a6e59c6c5dafa7c9
SND\aliaksandr_pedzko_cp <SND\aliaksandr_pedzko_cp@a83551a4-30f6-4d81-a974-c6ced450ddbf>
2010-05-05 13:15:57 +0000

    added missing test file.
    git-svn-id: https://kpnet.svn.codeplex.com/svn/trunk/KpNet@43583 a83551a4-30f6-4d81-a974-c6ced450ddbf

---
+++ Tests/KpNet.KdbPlusClient.Tests/KdbPlusConnectionStringBuilderTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace KpNet.KdbPlusClient.Tests
+{
+    [TestFixture]
+    public sealed class KdbPlusConnectionStringBuilderTest
+    {
+        [Test]
+        public void CorrectStringParseTest()
+        {
+            KdbPlusConnectionStringBuilder builder = new KdbPlusConnectionStringBuilder("server=1;port=2;user id=3;password=4;buffersize=16384");
+            Assert.AreEqual("1", builder.Server);
+            Assert.AreEqual(2, builder.Port);
+            Assert.AreEqual("3", builder.UserID);
+            Assert.AreEqual("4", builder.Password);
+            Assert.AreEqual(16384, builder.BufferSize);
+        }
+
+        [Test]
+        public void DifferentCaseStringParseTest()
+        {
+            KdbPlusConnectionStringBuilder builder = new KdbPlusConnectionStringBuilder("Server=1;Port=2;User ID=3;Password=4;buffersize=16384");
+            Assert.AreEqual("1", builder.Server);
+            Assert.AreEqual(2, builder.Port);
+            Assert.AreEqual("3", builder.UserID);
+            Assert.AreEqual("4", builder.Password);
+            Assert.AreEqual(16384, builder.BufferSize);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void EmptyStringParseTest()
+        {
+            new KdbPlusConnectionStringBuilder(null);
+        }
+    }
+}
