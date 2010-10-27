707a76de3baf7ecdd6f7e064a751dea54a57cf22
SND\aliaksandr_pedzko_cp <SND\aliaksandr_pedzko_cp@a83551a4-30f6-4d81-a974-c6ced450ddbf>
2010-10-27 09:23:31 +0000

    assemblyinfo was corrected
    git-svn-id: https://kpnet.svn.codeplex.com/svn/trunk/KpNet@57767 a83551a4-30f6-4d81-a974-c6ced450ddbf

--- Source/KpNet.Common/Properties/AssemblyInfo.cs
+++ Source/KpNet.Common/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -8,9 +7,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("KpNet.Common")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyCompany("http://kpnet.codeplex.com")]
 [assembly: AssemblyProduct("KpNet.Common")]
-[assembly: AssemblyCopyright("Copyright © EPAM Systems 2010")]
+[assembly: AssemblyCopyright("http://kpnet.codeplex.com/license")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.3.0.0")]
+[assembly: AssemblyFileVersion("2.3.0.0")]
--- Source/KpNet.Hosting/Properties/AssemblyInfo.cs
+++ Source/KpNet.Hosting/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("KpNet.Hosting")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyCompany("http://kpnet.codeplex.com")]
 [assembly: AssemblyProduct("KpNet.Hosting")]
-[assembly: AssemblyCopyright("Copyright © EPAM Systems 2010")]
+[assembly: AssemblyCopyright("http://kpnet.codeplex.com/license")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: InternalsVisibleTo("KpNet.Hosting.Tests")]
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.3.0.0")]
+[assembly: AssemblyFileVersion("2.3.0.0")]
